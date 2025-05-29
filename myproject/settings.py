c423a0391a147b0beba6ce1ebeb353684a69fd40
mircocazzaro <mirco.cazzaro.1999@gmail.com>
2025-05-29 13:09:39 +0200

    MINOR FIX

--- myproject/settings.py
+++ myproject/settings.py
@@ -66,3 +66,15 @@ LEVEL_DB = os.path.join(MEDIA_ROOT, 'level.duckdb')
 ALLOWED_DB = os.path.join(MEDIA_ROOT, 'allowed_queries.duckdb')
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
